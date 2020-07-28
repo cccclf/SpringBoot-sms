b967109d6e18be8e96840dbd25e3de8054b568f1
cccclf <774852667@qq.com>
2020-07-28 21:15:35 +0800

    增加员工

--- src/main/java/com/chen/controller/EmployeeController.java
+++ src/main/java/com/chen/controller/EmployeeController.java
@@ -1,10 +1,14 @@
 package com.chen.controller;
 
+import com.chen.dao.DepartmentDao;
 import com.chen.dao.EmployeeDao;
+import com.chen.pojo.Department;
 import com.chen.pojo.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Collection;
@@ -15,11 +19,31 @@ public class EmployeeController {
     @Autowired
     EmployeeDao employeeDao;
 
+    @Autowired
+    DepartmentDao departmentDao;
+
     @RequestMapping("/emps")
     public String list(Model model){
         Collection<Employee> employees = employeeDao.getAll();
         model.addAttribute("emps",employees);
         return "/emps/list";
     }
+
+    @GetMapping("/emp")
+    public String toAddPage(Model model){
+        //查处所有部门的信息
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("departments",departments);
+        return "emps/add";
+    }
+
+    @PostMapping("/emp")
+    public String addEmp(Employee employee){
+        //添加的操作
+        System.out.println("debug====>"+employee);
+        employeeDao.save(employee);
+        return "redirect:/emps";//重定向：url会发生改变
+    }
+
 
 }
